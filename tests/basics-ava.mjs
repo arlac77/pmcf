deef75b994575b74cde6ff1422a77e4d4667cfbf
Markus Felten <Markus.Felten@gmx.de>
2025-01-15 17:04:29 +0000

    test: deep structure

--- tests/basics-ava.mjs
+++ tests/basics-ava.mjs
@@ -1,13 +1,25 @@
 import test from "ava";
 import { World } from "../src/model.mjs";
 
-test("load Host", async t => {
+test("load location Host", async t => {
   const world = new World(new URL("fixtures/world1", import.meta.url).pathname);
 
   const host = await world.host("L1/host1");
   const loction = await world.location("L1");
 
   t.is(host.name, "L1/host1");
+  t.is(host.owner, world);
+  t.is(host.location, loction);
+  t.is(host.os, "linux");
+});
+
+test("load location network Host", async t => {
+  const world = new World(new URL("fixtures/world1", import.meta.url).pathname);
+
+  const host = await world.host("L1/n1/host2");
+  const loction = await world.location("L1");
+
+  t.is(host.name, "L1/n1/host2");
   t.is(host.owner, world);
   t.is(host.location, loction);
   t.is(host.os, "linux");
