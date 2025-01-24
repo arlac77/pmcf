529d459434223bc92c170e0ea75cede6ed04c21f
Markus Felten <Markus.Felten@gmx.de>
2025-01-24 16:48:31 +0000

    fix: split dns servers by priority < 10 => primary >=10 => fallback

--- src/dns.mjs
+++ src/dns.mjs
@@ -26,6 +26,10 @@ export class DNSService extends Base {
     }
   }
 
+  get domains() {
+    return [this.owner.domain];
+  }
+
   get propertyNames() {
     return ["recordTTL", "forwardsTo", "allowedUpdates"];
   }
