7c2e1c01917c7fe9fbce84f5e5e54523c02ab617
Markus Felten <Markus.Felten@gmx.de>
2025-01-30 12:49:19 +0000

    test: simple expand

--- tests/basics-ava.mjs
+++ tests/basics-ava.mjs
@@ -1,13 +1,5 @@
 import test from "ava";
-import {
-  Root,
-  Host,
-  Location,
-  Network,
-  Service,
-  Subnet,
-  Base
-} from "pmcf";
+import { Root, Host, Location, Network, Service, Subnet, Base } from "pmcf";
 
 test("types", t => {
   t.is(Root.types.location, Location);
@@ -18,12 +10,12 @@ test("types", t => {
 });
 
 test("Root basics", async t => {
-  const root = new Root("/somwhere");
+  const root = new Root("/somewhere");
   t.is(root.typeName, "root");
-  t.is(root.directory, "/somwhere");
+  t.is(root.directory, "/somewhere");
   t.is(root.name, "");
   t.is(root.fullName, "");
-  t.is(await root.load(""),root);
+  t.is(await root.load(""), root);
 });
 
 test("normalizeName", t => {
@@ -34,23 +26,33 @@ test("normalizeName", t => {
   t.is(Base.normalizeName("abc/def.json"), "abc");
 });
 
+
+test("expand", t => {
+  const root = new Root("/somewhere");
+  const l1 = new Location(root, { name: "l1" });
+
+  t.is(l1.expand("${directory}"),"/somewhere/l1");
+  t.is(l1.expand("${owner.directory}"),"/somewhere");
+ //t.is(l1.expand("${owner.domains}"),"/somewhere");
+});
+
 test("directory & name", t => {
-  const root = new Root("/somwhere");
+  const root = new Root("/somewhere");
 
   const l1 = new Location(root, { name: "l1" });
-  t.is(l1.directory, "/somwhere/l1");
+  t.is(l1.directory, "/somewhere/l1");
   t.is(l1.name, "l1");
   t.is(l1.fullName, "l1");
   t.is(l1.root, root);
 
   const h1 = new Location(l1, { name: "h1" });
-  t.is(h1.directory, "/somwhere/l1/h1");
+  t.is(h1.directory, "/somewhere/l1/h1");
   t.is(h1.name, "h1");
   t.is(h1.fullName, "l1/h1");
   t.is(h1.root, root);
 
   const h2 = new Location(l1, { name: "l2/h2" });
-  t.is(h2.directory, "/somwhere/l1/l2/h2");
+  t.is(h2.directory, "/somewhere/l1/l2/h2");
   t.is(h2.name, "l2/h2");
   t.is(h2.fullName, "l1/l2/h2");
   t.is(h2.root, root);
