85b2a9ba7e457d013482f930d907160546af8a47
Markus Felten <Markus.Felten@gmx.de>
2025-03-06 16:34:46 +0000

    fix: start generating cluster packages

--- src/cluster.mjs
+++ src/cluster.mjs
@@ -8,15 +8,14 @@ const ClusterTypeDefinition = {
   extends: Owner.typeDefinition,
   properties: {
     masters: { type: "host", collection: true, writeable: true },
-    backups: { type: "host", collection: true, writeable: true },
+    backups: { type: "host", collection: true, writeable: true }
   }
 };
 
 export class Cluster extends Owner {
-
   masters = new Set();
   backups = new Set();
-  
+
   static {
     addType(this);
   }
@@ -28,5 +27,36 @@ export class Cluster extends Owner {
   constructor(owner, data) {
     super(owner, data);
     this.read(data, ClusterTypeDefinition);
+  }
+
+  get packageName() {
+    return `${this.constructor.typeDefinition.name}-${this.owner.name}-${this.name}`;
+  }
+
+  async preparePackage(stagingDir) {
+    const result = await super.preparePackage(stagingDir);
+
+    const cfg = [
+      "vrrp_instance VI_1 {",
+      "  state MASTER",
+      "  interface end0",
+      "  virtual_router_id 101",
+      "  priority 255",
+      "  advert_int 1",
+      "  authentication {",
+      "    auth_type PASS",
+      "    auth_pass pass1234",
+      "  }",
+      "  virtual_ipaddress {",
+      "    192.168.1.250",
+      "  }",
+      "}"
+    ];
+
+    await writeLines(join(targetDir, "etc/keepalived"), "keepalived.conf", cfg);
+
+    result.properties.dependencies = ["keepalived"];
+
+    return result;
   }
 }
