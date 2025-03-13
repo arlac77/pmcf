d7e27977b901503f9abd422df38ab4e99064e25f
Markus Felten <Markus.Felten@gmx.de>
2025-03-13 00:28:05 +0000

    feat: split dns pkg into zone + config packages

--- src/dns.mjs
+++ src/dns.mjs
@@ -1,7 +1,12 @@
 import { join } from "node:path";
 import { createHmac } from "node:crypto";
 import { FileContentProvider } from "npm-pkgbuild";
-import { writeLines, isIPv6Address, normalizeIPAddress, isLinkLocal } from "./utils.mjs";
+import {
+  writeLines,
+  isIPv6Address,
+  normalizeIPAddress,
+  isLinkLocal
+} from "./utils.mjs";
 import { Base } from "./base.mjs";
 import { addType } from "./types.mjs";
 
@@ -12,7 +17,11 @@ const DNSServiceTypeDefinition = {
   properties: {
     hasSVRRecords: { type: "boolean", collection: false, writeable: true },
     hasCatalog: { type: "boolean", collection: false, writeable: true },
-    hasLinkLocalAdresses: { type: "boolean", collection: false, writeable: true },
+    hasLinkLocalAdresses: {
+      type: "boolean",
+      collection: false,
+      writeable: true
+    },
     notify: { type: "boolean", collection: false, writeable: true },
     recordTTL: { type: "string", collection: false, writeable: true },
     refresh: { type: "string", collection: false, writeable: true },
@@ -107,19 +116,68 @@ export class DNSService extends Base {
   }
 
   async *preparePackages(stagingDir) {
-    for await (const result of super.preparePackages(stagingDir)) {
-      await generateNamedDefs(this, stagingDir);
+    const name = this.owner.name;
+    const p1 = join(stagingDir, "p1");
 
-      result.properties.name = `named-${this.owner.name}`;
-      result.properties.dependencies = ["mf-named"];
-      result.properties.replaces = ["mf-named-zones"];
+    const result = {
+      sources: [new FileContentProvider(p1 + "/")[Symbol.asyncIterator]()],
+      outputs: this.outputs,
+      properties: {
+        name: `named-${name}`,
+        description: `named definitions for ${this.fullName}`,
+        access: "private"
+      }
+    };
 
-      result.sources.push(
-        new FileContentProvider(stagingDir + "/")[Symbol.asyncIterator]()
-      );
+    const options = ["forwarders {"];
+    for (const s of this.forwardsTo) {
+      for (const dns of s.findServices(DNS_SERVICE_FILTER)) {
+        options.push(...(dns.rawAddresses.map(a=>a+";")));
+      }
+    }
+    options.push("}");
+    await writeLines(join(p1, "etc/named.d/options"), `${name}.conf`, options);
 
+    const category = [];
+
+    /*
+    const category = ["acl trusted {"];
+
+    const network = this.owner.named("LOCAL");
+    for (const subnet of network.subnets()) {
+      category.push(`${subnet.name};`);
+    }
+
+    category.push("}");
+    */
+
+    await writeLines(
+      join(p1, "etc/named.d/categories"),
+      `${name}.conf`,
+      category
+    );
+
+    if(options.length > 2 || category.length > 2) {
       yield result;
     }
+
+    const p2 = join(stagingDir, "p2");
+
+    result.properties = {
+      name: `named-zones-${name}`,
+      description: `zone definitions for ${this.fullName}`,
+      dependencies: ["mf-named"],
+      replaces: ["mf-named-zones", `named-${name}`],
+      access: "private"
+    };
+
+    result.sources = [
+      new FileContentProvider(p2 + "/")[Symbol.asyncIterator]()
+    ];
+
+    await generateZoneDefs(this, p2);
+
+    yield result;
   }
 }
 
@@ -127,7 +185,7 @@ function fullName(name) {
   return name.endsWith(".") ? name : name + ".";
 }
 
-async function generateNamedDefs(dns, targetDir) {
+async function generateZoneDefs(dns, targetDir) {
   const ttl = dns.recordTTL;
   const updates = [Math.ceil(Date.now() / 1000), ...dns.soaUpdates].join(" ");
 
@@ -212,7 +270,10 @@ async function generateNamedDefs(dns, targetDir) {
     } of dns.owner.networkAddresses()) {
       const host = networkInterface.host;
 
-      if (!addresses.has(address) && (dns.hasLinkLocalAdresses || !isLinkLocal(address))) {
+      if (
+        !addresses.has(address) &&
+        (dns.hasLinkLocalAdresses || !isLinkLocal(address))
+      ) {
         addresses.add(address);
 
         zone.records.add(
@@ -278,7 +339,11 @@ async function generateNamedDefs(dns, targetDir) {
       if (zone.type !== "catalog") {
         const hash = createHmac("md5", zone.id).digest("hex");
         catalogZone.records.add(
-          createRecord(`${hash}.zones.catalog.${domain}.`, "PTR", fullName(zone.id))
+          createRecord(
+            `${hash}.zones.catalog.${domain}.`,
+            "PTR",
+            fullName(zone.id)
+          )
         );
       }
 
