705fb4b1a0ebe5e55cc9084c9d631cb1df870605
Markus Felten <Markus.Felten@gmx.de>
2025-03-14 21:18:54 +0000

    feat: list services

--- src/cli.mjs
+++ src/cli.mjs
@@ -2,10 +2,11 @@ import { parseArgs } from "node:util";
 import { argv, cwd, env } from "node:process";
 import { Root } from "./module.mjs";
 
-export async function prepare() {  
+export async function prepare(options={}) {  
   const { values, positionals } = parseArgs({
     args: argv.slice(2),
     options: {
+      ...options,
       verbose: {
         type: "boolean",
         short: "v",
