7ae8492ed1ca53e429e177f429e8ede673d6de14
Markus Felten <Markus.Felten@gmx.de>
2025-06-07 22:55:43 +0100

    test: bind basics

--- tests/bind-ava.mjs
+++ tests/bind-ava.mjs
@@ -1,5 +1,17 @@
 import test from "ava";
 import { Root, addresses } from "pmcf";
+import { BindService } from "../src/services/bind.mjs";
+
+test.only("BIND basics", async t => {
+  const root = new Root(new URL("fixtures/root1", import.meta.url).pathname);
+  await root.loadAll();
+
+  const bind = await root.named("/L1/C1/bind");
+
+  //console.log(bind.endpoints());
+
+  t.true(bind instanceof BindService);
+});
 
 test("BIND named", async t => {
   const root = new Root(new URL("fixtures/root1", import.meta.url).pathname);
