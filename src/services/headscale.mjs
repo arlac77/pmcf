a2a931828b8344fe9a34ea14363320de6e9bfb38
Markus Felten <Markus.Felten@gmx.de>
2025-10-07 09:23:21 +0100

    fix: more headscale endoints

--- src/services/headscale.mjs
+++ src/services/headscale.mjs
@@ -10,14 +10,26 @@ const HeadscaleServiceTypeDefinition = {
   service: {
     endpoints: [
       {
+        family: "unix",
+        path: "/run/headscale/headscale.sock"
+      },
+      {
         family: "IPv4",
         port: 8080,
         protocol: "tcp",
         tls: false
       },
       {
+        description: "grpc",
         family: "IPv4",
         port: 50443,
+        protocol: "tcp",
+        tls: false
+      },
+      {
+        description: "metrics debug",
+        family: "IPv4",
+        port: 9090,
         protocol: "tcp",
         tls: false
       }
