ebbc7df318eeb6e0ec1a6c8c043a4021dc49a971
Markus Felten <Markus.Felten@gmx.de>
2026-01-01 13:18:53 +0100

    fix: journald attributes

--- src/services/systemd-journal.mjs
+++ src/services/systemd-journal.mjs
@@ -13,14 +13,74 @@ const SystemdJournalServiceTypeDefinition = {
   owners: ServiceTypeDefinition.owners,
   key: "name",
   attributes: {
-    Compress: {
+    Storage: {
+      ...string_attribute_writable
+    },
+    Seal: {
+      ...string_attribute_writable
+    },
+    SplitMode: {
+      ...string_attribute_writable
+    },
+    SyncIntervalSec: {
+      ...duration_attribute_writable
+    },
+    RateLimitIntervalSec: {
+      ...duration_attribute_writable
+    },
+    RateLimitBurst: {
       ...string_attribute_writable
     },
     SystemMaxUse: {
       ...string_attribute_writable
     },
-    SyncIntervalSec: {
+    SystemKeepFree: {
+      ...string_attribute_writable
+    },
+    SystemMaxFileSize: {
+      ...string_attribute_writable
+    },
+    SystemMaxFiles: {
+      ...string_attribute_writable
+    },
+    RuntimeMaxUse: {
+      ...string_attribute_writable
+    },
+    RuntimeKeepFree: {
+      ...string_attribute_writable
+    },
+    RuntimeMaxFileSize: {
+      ...string_attribute_writable
+    },
+    RuntimeMaxFiles: {
+      ...string_attribute_writable
+    },
+    MaxRetentionSec: {
       ...duration_attribute_writable
+    },
+    MaxFileSec: {
+      ...duration_attribute_writable
+    },
+    ForwardToSyslog: {
+      ...string_attribute_writable
+    },
+    ForwardToKMsg: {
+      ...string_attribute_writable
+    },
+    ForwardToConsole: {
+      ...string_attribute_writable
+    },
+    ForwardToWall: {
+      ...string_attribute_writable
+    },
+    TTYPath: {
+      ...string_attribute_writable
+    },
+    MaxLevelStore: {
+      ...string_attribute_writable
+    },
+    Compress: {
+      ...string_attribute_writable
     }
   }
 };
