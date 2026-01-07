75f5deb87ad74cb7040a5b3e2cb4e5ea70d50e8f
Markus Felten <Markus.Felten@gmx.de>
2026-01-08 00:41:03 +0100

    fix:extendedProperty work around for systemd-resolved

--- src/services/systemd-resolved.mjs
+++ src/services/systemd-resolved.mjs
@@ -1,9 +1,14 @@
-import { addType, boolean_attribute_writable } from "pacc";
+import {
+  addType,
+  duration_attribute_writable,
+  string_attribute_writable,
+  boolean_attribute_writable
+} from "pacc";
 import {
   ExtraSourceService,
   ExtraSourceServiceTypeDefinition,
   ServiceTypeDefinition,
-  serviceEndpoints,
+  serviceEndpoints
 } from "pmcf";
 import { yesno } from "../utils.mjs";
 
@@ -14,8 +19,21 @@ const SystemdResolvedServiceTypeDefinition = {
   owners: ServiceTypeDefinition.owners,
   key: "name",
   attributes: {
-    dnssec: boolean_attribute_writable,
-    llmnr: boolean_attribute_writable
+    DNS: string_attribute_writable,
+    FallbackDNS: string_attribute_writable,
+    Domains: string_attribute_writable,
+    MulticastDNS: boolean_attribute_writable,
+    Cache: boolean_attribute_writable,
+    CacheFromLocalhost: boolean_attribute_writable,
+    DNSStubListener: boolean_attribute_writable,
+    DNSStubListenerExtra: string_attribute_writable,
+    ReadEtcHosts: boolean_attribute_writable,
+    ResolveUnicastSingleLabel: boolean_attribute_writable,
+    StaleRetentionSec: duration_attribute_writable,
+    RefuseRecordTypes: string_attribute_writable,
+    DNSSEC: boolean_attribute_writable,
+    DNSOverTLS: boolean_attribute_writable,
+    LLMNR: boolean_attribute_writable
   }
 };
 
@@ -59,9 +77,18 @@ export class SystemdResolvedService extends ExtraSourceService {
           DNS: serviceEndpoints(this, options(300, 399, 4)),
           FallbackDNS: serviceEndpoints(this, options(100, 199, 4)),
           Domains: [...this.localDomains].join(" "),
-          DNSSEC: yesno(this.dnssec),
           MulticastDNS: yesno(this.network.multicastDNS),
-          LLMNR: yesno(this.llmnr)
+
+          DNSSEC: yesno(this.DNSSEC),
+
+          // TODO extendet properties with getAttribute()
+          ...Object.fromEntries(
+            Object.entries(SystemdResolvedServiceTypeDefinition.attributes)
+              .map(([k, v]) => [k, this.extendedProperty(k)])
+              .filter(([k, v]) => v !== undefined)
+          ),
+          LLMNR: yesno(this.LLMNR),
+
         }
       ]
     };
