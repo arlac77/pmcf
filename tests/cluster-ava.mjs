7c5f4abee5e9f8cdfd9b6098d995b2a469bdcd24
Markus Felten <Markus.Felten@gmx.de>
2026-06-26 15:03:56 +0100

    test: wip

--- tests/cluster-ava.mjs
+++ tests/cluster-ava.mjs
@@ -4,12 +4,17 @@ import { assertObject } from "./util.mjs";
 import { root1 } from "./fixtures.mjs";
 
 test("Cluster basics", async t => {
-  const ic = new InitializationContext(new URL("fixtures/root1", import.meta.url).pathname);
+  const ic = new InitializationContext(
+    new URL("fixtures/root1", import.meta.url).pathname
+  );
   await ic.loadAll();
 
   const c1 = await ic.named("/L1/C1");
 
   const p1eth0 = await ic.named("/L1/host1/eth0");
+
+  t.is(p1eth0.network.name, "n1");
+  t.is(p1eth0.network.owner.name, "host1");
   t.true(c1.masters.indexOf(p1eth0) >= 0);
 
   await assertObject(t, await ic.named("/L1/C1"), root1(ic.root, "/L1/C1"));
