ab475d95da5b4c47b21fbdb171b78ce073fca946
Markus Felten <Markus.Felten@gmx.de>
2026-07-01 12:33:53 +0100

    test: ip46 link local subnets

--- tests/ethernet-network-interface-ava.mjs
+++ tests/ethernet-network-interface-ava.mjs
@@ -24,7 +24,7 @@ test("ethernet basics", t => {
     subnets: ["10.0.0.2/16"]
   });
 
-  t.deepEqual([...n1.subnets.keys()], ["10.0/16"]);
+  t.deepEqual([...n1.subnets.keys()], ["fe80::/64", "10.0/16"]);
 
   assign(networks_attribute, ic.root, n1);
   const h1 = new Host();
