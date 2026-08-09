cb932aec1617efd42b972ca3ba63d8a9f2acce74
Markus Felten <Markus.Felten@gmx.de>
2026-08-10 00:50:00 +0100

    fix: remove dup type attribute

--- src/service.mjs
+++ src/service.mjs
@@ -1,21 +1,13 @@
-import { string_attribute_writable } from "pacc";
 import { setBaseService, CoreService, addType } from "pmcf";
 
 export class Service extends CoreService {
   static name = "service";
-  static attributes = {
-    type: { ...string_attribute_writable, name: "type" }
-  };
   static {
     addType(this);
     setBaseService(this);
   }
 
   _type;
-
-  set type(value) {
-    this._type = value;
-  }
 
   set type(value) {
     this._type = value;
