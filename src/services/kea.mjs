d77ef5379687e8d5d1e93630dd971bdde6983594
Markus Felten <Markus.Felten@gmx.de>
2026-09-17 09:20:25 +0100

    fix: kea.service is invalid

--- src/services/kea.mjs
+++ src/services/kea.mjs
@@ -99,7 +99,6 @@ export class kea extends CoreService {
   };
   static service = {
     extends: ["dhcp"],
-    systemdService: "kea.service",
     systemUserName: "kea",
     systemGroupName: "kea",
     services: {
