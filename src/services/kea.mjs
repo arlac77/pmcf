2deb610de99d34589b41b479ea3532eb766f990f
Markus Felten <Markus.Felten@gmx.de>
2026-09-22 00:56:11 +0100

    fix: cleanup

--- src/services/kea.mjs
+++ src/services/kea.mjs
@@ -470,10 +470,6 @@ export class kea extends CoreService {
 
     const packageData = await this.preparePackage(dir);
 
-    asArray(this.keys).forEach(key => {
-      this.credentials.set(key.name, new credential(this,{name: key.name, algorithm: key.algorithm}))
-    });
-
     await this.writeSystemdCredentialConfig(dir, "kea-dhcp4.service");
     await this.writeSystemdCredentialConfig(dir, "kea-dhcp6.service");
     await this.writeSystemdCredentialConfig(dir, "kea-dhcp-ddns.service");
